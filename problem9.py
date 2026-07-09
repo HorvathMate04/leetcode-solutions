652c9042342ff85453ea214119899d8756b6030a
HorvathMate04 <culubpenguin@gmail.com>
2026-07-10 00:26:38 +0200

    problem9 alternate solve

--- problem9.py
+++ problem9.py
@@ -5,3 +5,16 @@ class Solution(object):
         if x < 0: return False
         if x < 10: return True
         return x - int(str(x)[::-1]) == 0
+    
+"""CHALLENGE: Could you solve it without converting the integer to a string?"""    
+    
+class Solution(object):
+    def isPalindrome2(self, x):
+        if x < 0: return False
+        if x < 10: return True
+        ori = x
+        rev = 0
+        while ori > 0:
+            rev = rev*10 + ori%10
+            ori = ori//10
+        return x-rev == 0
