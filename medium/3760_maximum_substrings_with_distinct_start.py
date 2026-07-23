df7426fced783b0983f113ca8f1552f5ed30a8c1
HorvathMate04 <culubpenguin@gmail.com>
2026-07-23 12:10:24 +0200

    problem3760 solved

---
+++ medium/3760_maximum_substrings_with_distinct_start.py
@@ -0,0 +1,13 @@
+"""You are given a string s consisting of lowercase English letters.
+
+Return an integer denoting the maximum number of substrings 
+you can split s into such that each substring starts with a distinct 
+character (i.e., no two substrings start with the same character)."""
+
+class Solution(object):
+    def maxDistinct(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        return len(set(s))
