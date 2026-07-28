50adf92ccfd7a52275914b969e7d7268ffae8aab
HorvathMate04 <culubpenguin@gmail.com>
2026-07-28 23:49:25 +0200

    problem2807 fixed misstype

--- medium/2807_insert_greatest_common_divisors_in_linked_list.py
+++ medium/2807_insert_greatest_common_divisors_in_linked_list.py
@@ -31,7 +31,7 @@ class Solution(object):
     def findGreatestCommonDivisor(self, num1, num2):
         div = 1
         i = 1
-        while i <= num1 and div <= num2:
+        while i <= num1 and i <= num2:
             if num1 % i == 0 and num2 % i == 0:
                 div = i
             i += 1
